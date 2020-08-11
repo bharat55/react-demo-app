b49c6c014e373147e0b4a38bbaad6cfc0facb1b4
Bharat <bharat.rathor.5074@gmail.com>
2020-08-11 20:58:31 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact'
 import NavBar from './components/layoutes/NavBar';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/about" component={About}/>
+          <Route exact path="/contact" component={Contact}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
